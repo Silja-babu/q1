698cd55e5b7c158485e71be48d2cd9b3888c45dd
Silja-Babu <siljababu29@gmail.com>
2024-12-03 16:52:50 +0530

    q1

---
+++ q1.js
@@ -0,0 +1,40 @@
+function checking() {
+    let palin = prompt();
+    let palins = palin.toLowerCase().replace(/[^a-z0-9]/gi, '');
+    let palindrome = palins.split('').reverse().join('');
+    if (palins === palindrome) {
+        return true;
+    } else {
+        return false;
+    }
+}
+alert(checking());  
+
+
+
+
+/*function checking(){
+    let palin = prompt()
+    let palins = palin.toLowerCase().replace(/[^a-z0-9]/gi,'');
+  let palindrome = palins.split('').reverse().join('')
+
+if(palin===palindrome){
+    return true
+}else{
+    return false
+}
+}
+console.log(checking())*/
+
+
+/*function palindrome(str){
+    let palin = str.split("").reverse().join("");
+
+    if(palin === str){
+        return true
+    }else{
+        return false;
+    }
+}
+console.log(palindrome("eye"))
+palindrome("step not on pets")*/
